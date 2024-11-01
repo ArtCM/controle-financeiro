22f4570d02b67661270596d42a1b5cd990c82a18
ArtCM <arthurperfilprof@hotmail.com>
2024-11-01 16:52:33 -0300

    feat: finalizado o projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@
 import AddExpense from "../components/AddExpense/AddExpense";
 import { Expense } from "@/types/expense";
 import { useState, useEffect } from "react";
+import Trash from "../../public/assets/images/trash.svg"
+
+import './style.css';
+import Image from "next/image";
 
 export default function Home() {
 
@@ -28,28 +32,68 @@ export default function Home() {
     fetchExpenses();
   };
 
+  // Delete expense
+  const handleDeleteExpense = async (id: number) => {
+    const confirmed = confirm("Tem certeza que deseja excluir esta despesa?");
+    if (confirmed) {
+        await fetch(`/api/expenses/${id}`, {
+            method: 'DELETE',
+        });
+        fetchExpenses();
+    }
+  };
+
   return (
     <>
-      <h1>Despesas</h1>
+      <main>
+        <h1>Despesas</h1>
 
-      <button onClick={() => setShowAddExpense(true)}>Adicionar Despesa</button>
+        <button className="add-button" onClick={() => setShowAddExpense(true)}>Adicionar Despesa</button>
 
-      {/* Component to add expenses */}
-      {showAddExpense && (
-        <AddExpense
-          onAddExpense={handleAddExpense}
-          onClose={() => setShowAddExpense(false)}
-        />
-      )}
+        {/* Component to add expenses */}
+        {showAddExpense && (
+          <AddExpense
+            onAddExpense={handleAddExpense}
+            onClose={() => setShowAddExpense(false)}
+          />
+        )}
 
-      <div>
-        <h2>Lista de Despesas</h2>
-        {expenses.map(expense => (
-          <div key={expense.id}>
-            <p>{expense.category} - R$ {(expense.amount / 100).toFixed(2)} - {expense.date}</p>
-          </div>
-        ))}
-      </div>
+        <div className="expenses-list container">
+          <h2>Lista de Despesas</h2>
+          <table className="expenses-list__container">
+            <thead>
+              <tr>
+                <th>Categoria</th>
+                <th>Valor</th>
+                <th>Data</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+            {expenses.length === 0 ? (
+              <tr>
+                <td colSpan={4} className="text-center">
+                  Nenhuma despesa adicionada ainda.
+                </td>
+              </tr>
+            ) : (
+              expenses.map(expense => (
+                <tr key={expense.id} className="expenses-list__container--item">
+                  <td>{expense.category}</td>
+                  <td>R$ {(expense.amount / 100).toFixed(2)}</td>
+                  <td>{new Date(expense.date).toLocaleDateString('pt-BR')}</td>
+                  <td>
+                    <button onClick={() => handleDeleteExpense(expense.id)}>
+                      <Image src={Trash} alt="Trash" />
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+            </tbody>
+          </table>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/AddExpense/AddExpense.tsx
+++ src/components/AddExpense/AddExpense.tsx
@@ -17,45 +17,73 @@ export default function AddExpense({ onAddExpense, onClose }: AddExpenseProps) {
         date: '',
     });
 
+    const [value, setValue] = useState("");
+
+    const handleCategoryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const sanitizedValue = e.target.value.replace(/[^a-zA-Z0-9 ]/g, "");
+        setForm({ ...form, category: sanitizedValue });
+    };
+
+    const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = e.target.value.replace(/\D/g, "");
+
+        if (inputValue === "") {
+            setValue("");
+            return;
+        }
+
+        const parsedValue = (parseFloat(inputValue) / 100).toFixed(2);
+        
+        setValue(parsedValue);
+    };
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
         const newExpense: Expense = {
-          ...form,
-          amount: Math.round(form.amount * 100), 
+            ...form,
+            amount: Math.round(parseFloat(value.replace(",", "."))),
         };
     
         onAddExpense(newExpense);
 
         setForm({ category: '', amount: 0, date: '' });
-
+        setValue("");
         onClose();
-      };
+    };
     
     return (
         <div className="add-expense">
-            <button onClick={onClose} className="close-button">X</button>
-            
-            <h2>Adicionar Despesa</h2>
+            <div className='add-expense__top'>
+                <h2>Adicionar Despesa</h2>
 
-            <form onSubmit={handleSubmit}>
+                <button onClick={onClose} className="close-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
+                </button>
+            </div>
+
+            <form className='add-expense__form' onSubmit={handleSubmit}>
                 <input
-                type="text"
-                placeholder="Categoria"
-                value={form.category}
-                onChange={(e) => setForm({ ...form, category: e.target.value })}
+                    type="text"
+                    placeholder="Categoria"
+                    value={form.category}
+                    onChange={handleCategoryChange}
+                    required
                 />
                 <input
-                type="number"
-                placeholder="Valor"
-                value={form.amount}
-                onChange={(e) => setForm({ ...form, amount: parseFloat(e.target.value) })}
+                    type="text"
+                    placeholder="R$ 0,00"
+                    value={value ? `R$ ${value.replace(".", ",")}` : ""}
+                    onChange={handleAmountChange}
+                    required
                 />
                 <input
-                type="date"
-                value={form.date}
-                onChange={(e) => setForm({ ...form, date: e.target.value })}
+                    type="date"
+                    value={form.date}
+                    onChange={(e) => setForm({ ...form, date: e.target.value })}
+                    required
                 />
-                <button type="submit">Adicionar Despesa</button>
+                <button type="submit">Adicionar</button>
             </form>
         </div>
     );

5f2bdad910ae823258d50e7baefbb7f6e441c1d9
ArtCM <arthurperfilprof@hotmail.com>
2024-11-01 17:43:06 -0300

    fix: fix deploy bug

--- src/app/api/expenses/[id]/route.ts
+++ src/app/api/expenses/[id]/route.ts
@@ -4,21 +4,13 @@ import { ExpenseModel } from '../../../../models/ExpenseModel';
 export async function PUT(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
 
-  if (!id) {
-    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
-  }
-
   const { category, amount, date }: { category: string; amount: number; date: string } = await request.json();
-
-  const updatedExpense = await ExpenseModel.update(parseInt(id), {
+  
+  const updatedExpense = await ExpenseModel.update(parseInt(id, 10), { 
     category,
-    amount: amount * 100,
+    amount: amount * 100, 
     date: new Date(date),
   });
-
-  if (!updatedExpense) {
-    return NextResponse.json({ error: 'Expense not found' }, { status: 404 });
-  }
 
   return NextResponse.json(updatedExpense);
 }
@@ -26,10 +18,7 @@ export async function PUT(request: Request, { params }: { params: { id: string }
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
 
-  if (!id) {
-    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
-  }
-
-  await ExpenseModel.delete(parseInt(id));
+  await ExpenseModel.delete(parseInt(id, 10)); 
+  
   return NextResponse.json(null, { status: 204 });
 }
